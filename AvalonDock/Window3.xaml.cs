0956215749f10395143b54b10ac02351fc227a50
射落白羊 <517003047@qq.com>
2016-12-08 18:15:42 +0800

    v2.1

---
+++ AvalonDock/Window3.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AvalonDock
+{
+    /// <summary>
+    /// Window3.xaml 的交互逻辑
+    /// </summary>
+    public partial class Window3 : Window
+    {
+
+        private ObservableCollection<Warship> warships = new ObservableCollection<Warship>();
+        public Window3()
+        {
+            InitializeComponent();
+        }
+
+        private void button_turnRight_Click(object sender, RoutedEventArgs e)
+        {
+            Storyboard _storyboard = new Storyboard();
+            Storyboard _xstoryboard = new Storyboard();
+            Storyboard _ystoryboard = new Storyboard();
+
+            TimeSpan span = TimeSpan.FromSeconds(0.1);
+            for(int i = 0; i < 10; i++)
+            {
+                DoubleAnimation _animation = new DoubleAnimation();
+                Storyboard.SetTarget(_animation, ws);
+                Storyboard.SetTargetProperty(_animation, new PropertyPath("Course"));
+                _animation.Duration = span;
+                _animation.To = ws.Course - 9.0*(i+1);
+                _animation.BeginTime = TimeSpan.FromSeconds(0.1 * i);
+                _storyboard.Children.Add(_animation);
+
+                double angle = Math.PI * (i+1)*9 / 180.0;
+                double x = Math.Sin(angle) * 10.0;
+                double y = Math.Cos(angle) * 10.0;
+
+                DoubleAnimation _xanimation = new DoubleAnimation();
+                Storyboard.SetTarget(_xanimation, ws);
+                Storyboard.SetTargetProperty(_xanimation, new PropertyPath("(Canvas.Left)"));
+                _xanimation.Duration = span;
+
+                DoubleAnimation _yanimation = new DoubleAnimation();
+                Storyboard.SetTarget(_yanimation, ws);
+                Storyboard.SetTargetProperty(_yanimation, new PropertyPath("(Canvas.Bottom)"));
+                _yanimation.Duration = span;
+
+                _xanimation.To = Canvas.GetLeft(ws) + x;
+                _yanimation.To = Canvas.GetBottom(ws) + y;
+                _xanimation.BeginTime = TimeSpan.FromSeconds(0.1 * i);
+                _yanimation.BeginTime = TimeSpan.FromSeconds(0.1 * i);
+
+                _xstoryboard.Children.Add(_xanimation);
+                _ystoryboard.Children.Add(_yanimation);
+            }
+            _storyboard.Begin();
+            _xstoryboard.Begin();
+            _ystoryboard.Begin();
+        }
+    }
+}
---
+++ Serial/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Serial
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Baowen a = new Baowen("a","a","a");
+            Baowen b = new Baowen("b", "b", "a");
+            Baowen c = new Baowen("c", "c", "a");
+            List<Baowen> listB = new List<Baowen>();
+            listB.Add(a);
+            listB.Add(b);
+            listB.Add(c);
+            listBox.DataContext = listB;
+        }
+    }
+
+    class Baowen
+    {
+        private int id;
+        private string bw;
+        private string pinyin;
+        private string qianming;
+
+        public Baowen()
+        {
+            this.bw = null;
+            this.pinyin = null;
+            this.qianming = null;
+        }
+
+        public Baowen(string bw, string pinyin, string qianming)
+        {
+            this.bw = bw;
+            this.pinyin = pinyin;
+            this.qianming = qianming;
+        }
+
+
+        public string Sign
+        {
+            get
+            {
+                string temp = pinyin.Replace('\'', '@');
+                temp = temp.Replace('_', '[');
+                temp = temp + " @ " + qianming;
+                return temp;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+
+        public string Pinyin
+        {
+            get
+            {
+                return pinyin;
+            }
+
+            set
+            {
+                string temp = value.Replace("/", "__");
+                string pattern = "\\W+";
+                Regex rgx = new Regex(pattern);
+                temp = rgx.Replace(value, " ");
+                pinyin = temp.Replace("__", "/");
+            }
+        }
+
+        public string Qianming
+        {
+            get
+            {
+                return qianming;
+            }
+
+            set
+            {
+                qianming = value;
+            }
+        }
+
+        public string Bw
+        {
+            get
+            {
+                return bw;
+            }
+
+            set
+            {
+                bw = value;
+            }
+        }
+    }
+}
